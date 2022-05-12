aae72b0ff9c6c4fa1612f5ecda87d38a014ae6ef
James <poyuan1004@gmail.com>
2022-05-12 00:09:04 -0400

    configure postgres db v2

--- app/core/models.py
+++ app/core/models.py
@@ -8,7 +8,7 @@ class UserManager(BaseUserManager):
     def create_user(self, email, password=None, **extra_fields):
         """Creates and saves a new User"""
         if not email:
-            raise ValueError('Users ')
+            raise ValueError('Users must have an email')
         user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
